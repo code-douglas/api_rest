0159e8019a32a51f5b7db98872c31ad2af97e186
code-douglas <douglasd3v.contato@gmail.com>
2024-05-12 00:08:49 -0300

    Configurando Sequelize

--- src/controllers/Home.js
+++ src/controllers/Home.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class Home {
-  index(req, res) {
-    res.json({
-      'Tudo certo': true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Douglas',
+      sobrenome: 'de Souza',
+      email: 'douglas@gmail.com',
+      idade: 21,
+      peso: 120,
+      altura: 1.80,
     });
+    res.json(novoAluno);
   }
 }
 
