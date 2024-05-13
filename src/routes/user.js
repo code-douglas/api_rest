3346242e293d59215d2f83e18a8c62b3060018c5
code-douglas <douglasd3v.contato@gmail.com>
2024-05-12 22:10:49 -0300

    Adicionando Middleware de verificação do JWT

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import userController from '../controllers/User';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
-router.get('/', userController.index);
+router.get('/', loginRequired, userController.index);
 router.post('/', userController.store);
-router.get('/:id', userController.show);
-router.put('/:id', userController.update);
-router.delete('/:id', userController.delete);
+router.get('/:id', loginRequired, userController.show);
+router.put('/:id', loginRequired, userController.update);
+router.delete('/:id', loginRequired, userController.delete);
 export default router;
